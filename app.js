ba12adf3a6f4ad93942510eeb03441261fb7898e
indeed <bhojrajpathak001@gmail.com>
2024-04-10 01:35:04 +0530

    form action routing request

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
-//   res.setHeader("Content-Type", "text/html");
-  res.write("<html>");
-  res.write("<head><title>My firset page</title></head>");
-  res.write("<body>Hello from node </body>");
-  res.write("</html>");
+  const url = req.url;
+  if (url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>Enter message</title></head>");
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"/><button>send</button> </form> </body>'
+    );
+    res.write("</html>");
+    return res.end();
+  }
+  if (url === "/message") {
+    console.log();
+    res.write("<h1>bhojraj</h1>");
+  }
 });
 
 server.listen(3000);
