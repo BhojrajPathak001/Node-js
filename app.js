232cad81a6028ceb13c180ce7f0f4d42a4175210
indeed <bhojrajpathak001@gmail.com>
2024-04-10 01:21:48 +0530

    response in node server request

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method, req.headers);
-  //   process.exit();
+//   res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My firset page</title></head>");
+  res.write("<body>Hello from node </body>");
+  res.write("</html>");
 });
 
 server.listen(3000);
