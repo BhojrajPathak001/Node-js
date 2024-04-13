58c99c528e4b71877918bdd8fb02216927464f3c
indeed <bhojrajpathak001@gmail.com>
2024-04-13 22:12:45 +0530

    separting routing concersn

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 bodyParser = require("body-parser");
 const adminRoutes = require("./routes/admin");
+const shopsRoutes = require("./routes/shops");
 
 // support parsing of application/json type post data
 app.use(bodyParser.json());
@@ -9,16 +10,7 @@ app.use(bodyParser.json());
 //support parsing of application/x-www-form-urlencoded post data
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// app.use(adminRoutes);
-
-// app.use("/", (req, res, next) => {
-//   console.log("parent route");
-//   next();
-//   // res.send(`<h1>Hello frome Express!</h1>`);
-// });
-app.use((req, res, next) => {
-  console.log("route someting");
-  res.send(`<h1>hello from middlware</h1>`);
-});
+app.use(adminRoutes);
+app.use(shopsRoutes);
 
 app.listen(3000); //this is same as create server and then listen to the port as in node js
