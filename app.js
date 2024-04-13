40a0e7ce25aeecac9a3535eee833332500a3a939
indeed <bhojrajpathak001@gmail.com>
2024-04-14 03:56:23 +0530

    css added to html pages

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ bodyParser = require("body-parser");
 const adminRoutes = require("./routes/admin");
 const shopsRoutes = require("./routes/shops");
 const path = require("path");
-
 // support parsing of application/json type post data
 app.use(bodyParser.json());
 
@@ -15,7 +14,7 @@ app.use("/admin", adminRoutes);
 app.use(shopsRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "notFound.html"));
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(3000); //this is same as create server and then listen to the port as in node js
