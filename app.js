db9dfc6da70468905064af59017d2c2cf284ee95
indeed <bhojrajpathak001@gmail.com>
2024-04-14 19:14:23 +0530

    view using template engines

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
-const express = require("express");
-const app = express();
-bodyParser = require("body-parser");
-const adminRoutes = require("./routes/admin");
-const shopsRoutes = require("./routes/shops");
-const path = require("path");
-// support parsing of application/json type post data
-app.use(bodyParser.json());
+const path = require('path');
 
-//support parsing of application/x-www-form-urlencoded post data
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
-app.use("/admin", adminRoutes);
-app.use(shopsRoutes);
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
 
-app.listen(3000); //this is same as create server and then listen to the port as in node js
+app.listen(3000);
