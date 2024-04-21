43d9c4e924ef18a6f7067f8dbd93e9a954709970
indeed <bhojrajpathak001@gmail.com>
2024-04-21 20:59:52 +0530

    progess with sequelize

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const errorController = require("./controllers/error");
-const db = require("./util/database");
+const sequelize = require("./util/database");
 
 const app = express();
 
@@ -19,5 +19,14 @@ app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
+
+sequelize
+  .sync()
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(3000);
--- util/database.js
+++ util/database.js
@@ -1,11 +1,7 @@
-const mysql = require("mysql2");
-
-const pool = mysql.createPool({
-  connectionLimit: 10,
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize("node_complete", "root", "997760", {
+  dialect: "mysql",
   host: "localhost",
-  user: "root",
-  password: "997760",
-  database: "node_complete",
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
