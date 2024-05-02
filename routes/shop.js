efca0aff2e86b916b57e060b3fcad10cf36ab4c1
indeed <bhojrajpathak001@gmail.com>
2024-05-02 13:39:47 +0530

    delete from the cart

--- routes/shop.js
+++ routes/shop.js
@@ -13,7 +13,7 @@ router.get("/cart", shopController.getCart);
 
 router.post("/cart", shopController.postCart);
 
-// router.post("/cart-delete-item", shopController.postCartDeleteProudct);
+router.post("/cart-delete-item", shopController.postCartDeleteProudct);
 
 // router.post("/create-order", shopController.postOrder);
 
