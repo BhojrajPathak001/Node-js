7dd49a070d94cd9229877c51af88cbbdebfc2f5d
indeed <bhojrajpathak001@gmail.com>
2024-05-03 22:36:15 +0530

    started with mongoose

--- app.js
+++ app.js
@@ -2,11 +2,10 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const errorController = require("./controllers/error");
-const mongoConnect = require("./util/database").mongoConnect;
 const adminRoutes = require("./routes/admin");
 const app = express();
 const shopRoutes = require("./routes/shop");
-const User = require("./models/user");
+const mongoose = require("mongoose");
 
 app.set("view engine", "ejs");
 app.set("views", "views");
@@ -14,22 +13,30 @@ app.set("views", "views");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use((req, res, next) => {
-  User.findById("66304071a958003f3a9e912a")
-    .then((user) => {
-      req.user = new User(user.name, user.email, user._id, user.cart);
-      next();
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+// app.use((req, res, next) => {
+//   User.findById("66304071a958003f3a9e912a")
+//     .then((user) => {
+//       req.user = new User(user.name, user.email, user._id, user.cart);
+//       next();
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// });
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
-  app.listen(3000);
-});
+mongoose
+  .connect(
+    `mongodb+srv://bhojrajpathak:kcucA3wliearQpM4@cluster0.90qm6ik.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0`
+  )
+  .then((result) => {
+    app.listen(3000);
+    console.log("mongodb connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
--- models/product.js
+++ models/product.js
@@ -1,79 +1,101 @@
-const mongodb = require("mongodb");
-const { getDb } = require("../util/database");
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-class Product {
-  constructor(title, price, imageUrl, description, userId) {
-    this.title = title;
-    this.price = price;
-    this.imageUrl = imageUrl;
-    this.description = description;
-    this.userId = userId;
-  }
-  save() {
-    const db = getDb();
-    return db
-      .collection("products")
-      .insertOne(this)
-      .then((result) => {
-        console.log(result, "product.js");
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }
+const productSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+});
 
-  static fetchAll() {
-    const db = getDb();
-    return db
-      .collection("products")
-      .find() //yeh cursor return krta jo ki ek obj hai prov by mongodb yeh data ko step by step retrive krta hai
-      .toArray() //small data chaheye tou directly toArray method use kr skte hai yeh phir promise return krega
-      .then((products) => {
-        return products;
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }
-  static findById(id) {
-    const db = getDb();
-    return db
-      .collection("products")
-      .find({ _id: new mongodb.ObjectId(id) })
-      .next()
-      .then((product) => {
-        return product;
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }
-  static deletebyId(id) {
-    const db = getDb();
-    return db
-      .collection("products")
-      .deleteOne({ _id: new mongodb.ObjectId(id) })
-      .then(() => {
-        console.log("Product deleted");
-        return;
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }
-  static updateById(id, updatedProduct) {
-    const db = getDb();
-    return db
-      .collection("products")
-      .updateOne({ _id: new mongodb.ObjectId(id) }, { $set: updatedProduct })
-      .then(() => {
-        console.log("proudct updated successfully");
-        return;
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }
-}
+// const mongodb = require("mongodb");
+// const { getDb } = require("../util/database");
 
-module.exports = Product;
+// class Product {
+//   constructor(title, price, imageUrl, description, userId) {
+//     this.title = title;
+//     this.price = price;
+//     this.imageUrl = imageUrl;
+//     this.description = description;
+//     this.userId = userId;
+//   }
+//   save() {
+//     const db = getDb();
+//     return db
+//       .collection("products")
+//       .insertOne(this)
+//       .then((result) => {
+//         console.log(result, "product.js");
+//       })
+//       .catch((err) => {
+//         console.log(err);
+//       });
+//   }
+
+//   static fetchAll() {
+//     const db = getDb();
+//     return db
+//       .collection("products")
+//       .find() //yeh cursor return krta jo ki ek obj hai prov by mongodb yeh data ko step by step retrive krta hai
+//       .toArray() //small data chaheye tou directly toArray method use kr skte hai yeh phir promise return krega
+//       .then((products) => {
+//         return products;
+//       })
+//       .catch((err) => {
+//         console.log(err);
+//       });
+//   }
+//   static findById(id) {
+//     const db = getDb();
+//     return db
+//       .collection("products")
+//       .find({ _id: new mongodb.ObjectId(id) })
+//       .next()
+//       .then((product) => {
+//         return product;
+//       })
+//       .catch((err) => {
+//         console.log(err);
+//       });
+//   }
+//   static deletebyId(id) {
+//     const db = getDb();
+//     return db
+//       .collection("products")
+//       .deleteOne({ _id: new mongodb.ObjectId(id) })
+//       .then(() => {
+//         console.log("Product deleted");
+//         return;
+//       })
+//       .catch((err) => {
+//         console.log(err);
+//       });
+//   }
+//   static updateById(id, updatedProduct) {
+//     const db = getDb();
+//     return db
+//       .collection("products")
+//       .updateOne({ _id: new mongodb.ObjectId(id) }, { $set: updatedProduct })
+//       .then(() => {
+//         console.log("proudct updated successfully");
+//         return;
+//       })
+//       .catch((err) => {
+//         console.log(err);
+//       });
+//   }
+// }
+
+module.exports = mongoose.model("product", productSchema);
